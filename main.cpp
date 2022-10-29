1bd290dbf2a5a604f895ccc7bf3d826eb73e0cb4
Tiollit <Tiollit7@gmail.com>
2022-10-29 09:20:42 -0700

    Lab 1 b

--- main.cpp
+++ main.cpp
@@ -1,124 +1,79 @@
 #include <iostream>
 #include <ctime>
+#include <string>
 using namespace std;
-//void PrintArray(int arr[], const uint32_t size)
-//{
-//  for (size_t i = 0; i < size; i++)
-//  {
-//      cout << arr[i] << "\t";
-//  }cout << endl;
-//}
-//void PrintArray(double arr[], const uint32_t size)
-//{
-//  for (size_t i = 0; i < size; i++)
-//  {
-//      cout << arr[i] << "\t";
-//  }cout << endl;
-//}
-//void PrintArray(char arr[], const uint32_t size)
-//{
-//  for (size_t i = 0; i < size; i++)
-//  {
-//      cout << arr[i] << "\t";
-//  }cout << endl;
-//}
-template<typename T>
-T Rand(char trigger = 'i')
+int LeapYear(int y1, int Month[1], int year)
 {
-    switch (trigger)
-    {
-    case 'i':
-    {
-        return rand() % 10;
-    }break;
-    case 'd':
-    {
-        return (1000 + rand() % 9999) * 0.1;
-    }break;
-    case 'c':
-    {
-        return 100 + rand() % 255;
-    }break;
-    }
+	if (y1 % 4 == 0)
+	{
+		Month[1] = 29;
+		year = 365;
+	}
+	else
+	{
+		Month[1] = 28;
+		year = 364;
+	}
+	return 0;
 }
-template <typename T>
-void RandArray(T arr[], const uint32_t size)
-{
-    char trigger = '0';
-    if (typeid(arr[0]) == typeid(int))
-    {
-        trigger = 'i';
-    }
-    if (typeid(arr[0]) == typeid(double))
-    {
-        trigger = 'd';
-    }
-    if (typeid(arr[0]) == typeid(char))
-    {
-        trigger = 'c';
-    }
-    for (size_t i = 0; i < size; i++)
-    {
-        arr[i] = Rand<T>(trigger);
-    }
+int Final(int d1, int d2, int d3)
+{	
+	
 }
-template <typename T>
-void RandArray(T arr[][5], const uint32_t col, const uint32_t row)
-{
-    char trigger = '0';
-    if (typeid(arr[0][0]) == typeid(int))
-    {
-        trigger = 'i';
-    }
-    if (typeid(arr[0][0]) == typeid(double))
-    {
-        trigger = 'd';
-    }
-    if (typeid(arr[0][0]) == typeid(char))
-    {
-        trigger = 'c';
-    }
-    for (size_t i = 0; i < col; i++)
-    {
-        for (size_t j = 0; j < row; j++)
-        {
-            arr[i][j] = Rand<T>(trigger);
-        }
-    }
-}
-template <typename T>
-void PrintArray(T arr[][5], const uint32_t col, const uint32_t row)
-{
-    for (size_t i = 0; i < col; i++)
-    {
-        for (size_t j = 0; j < row; j++)
-        {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-template <typename T>
-void PrintArray(T arr[], const uint32_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arr[i] << "\t";
-    }cout << endl;
-}
+
+
 int main()
 {
-    srand(time(0));
-    const uint32_t size = 5;
-    int arr_i[size];
-    RandArray<int>(arr_i, size);
-    PrintArray<int>(arr_i, size);
-    cout << endl;
-    double arr_d[size][size];
-    RandArray<double>(arr_d, size, size);
-    PrintArray<double>(arr_d, size, size);
-    return 0;
+	int d1, d2, m1, y1, m2, y2, year, month = 0, day = 0, sum = 0;
+	int Month[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };	
+	cout << "Incert 2 dates in format dd mm yy:" << endl;
+	cout << "Incert 1st date: " << endl;
+	cin >> d1 >> m1 >> y1;
+	cout << "Incert 2d date: " << endl;
+	cin >> d2 >> m2 >> y2;	   
+	for (int i = y1; i <= y2; i++)
+	{
+		LeapYear(i, Month, year);
+		sum += year;
+		if (m2 > m1)
+		{			
+			for (int j = m1; j < m2; j++)
+			{
+				month = month + Month[j - 1];
+				if (d1 > d2 && j = m1)
+				{
+					day = day + Month[j - 1] - d1 + d2;
+					month = month - Month[j - 1];
+				}
+				else if (d1 < d2 && j = m1)
+				{
+					day = day - d1 + d2;
+				}							
+			}
+		}
+		if (m2 < m1)
+		{
+			for (int j = m1; j <= 12; j++)
+			{
+				month = month + Month[j - 1];
+				if (d1 > d2 && j = m1)
+				{
+					day = day + Month[j - 1] - d1 + d2;
+					month = month - Month[j - 1];
+				}
+				else if (d1 < d2 && j = m1)
+				{
+					day = day - d1 + d2;
+				}
+			}
+			for (int j = 0; j < m2; j++)
+			{
+				month = month + Month[j-1];
+			}
+		}
+	}
+	sum = sum + month + day;
+	return 0;
 }
 
 
