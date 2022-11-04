416f7031b4f29d2a2b51299cfe8e6a5078f4afe1
Tiollit <Tiollit7@gmail.com>
2022-11-03 22:21:19 -0700

    Lab 1 b

--- main.cpp
+++ main.cpp
@@ -2,81 +2,84 @@
 #include <ctime>
 #include <string>
 using namespace std;
-int LeapYear(int y1, int Month[1], int year)
+int LeapYear(int i)
 {
-	if (y1 % 4 == 0)
+	int y;
+	int month[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
+	if (i % 4 == 0)
 	{
-		Month[1] = 29;
-		year = 365;
+		y = 365;
+		month[1] = 29;
 	}
-	else
-	{
-		Month[1] = 28;
-		year = 364;
-	}
+	else y = 364;
 	return 0;
-}
-int Final(int day, int month, int year)
-{	
-	int d1, d2, m1, y1, m2, y2, year, sum;
-	int Month[12];
-	for (int i = y1; i <= y2; i++)
-	{
-		LeapYear(i, Month, year);
-		sum += year;
-		if (m2 > m1)
-		{
-			for (int j = m1; j < m2; j++)
-			{
-				month = month + Month[j - 1];
-				if (d1 > d2 && j = m1)
-				{
-					day = day + Month[j - 1] - d1 + d2;
-					month = month - Month[j - 1];
-				}
-				else if (d1 < d2 && j = m1)
-				{
-					day = day - d1 + d2;
-				}
-			}
-		}
-		if (m2 < m1)
-		{
-			for (int j = m1; j <= 12; j++)
-			{
-				month = month + Month[j - 1];
-				if (d1 > d2 && j = m1)
-				{
-					day = day + Month[j - 1] - d1 + d2;
-					month = month - Month[j - 1];
-				}
-				else if (d1 < d2 && j = m1)
-				{
-					day = day - d1 + d2;
-				}
-			}
-			for (int j = 0; j < m2; j++)
-			{
-				month = month + Month[j - 1];
-			}
-		}
-	}
-	sum = sum + month + day;
-	return 0;
-	
 }
 
+int HowManyDays(int d1, int d2, int m1, int m2, int y1, int y2)
+{
+	int month[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int y, days = 0, months = 0, years = 0, sum = 0;
+	if (y1 == y2)
+	{
+		LeapYear(y1);
+		if (m2 > m1)
+		{
+			for (int i = m1; i <= m2; i++)
+			{				
+				if (i == m1) 
+				{
+					days = days + month[i - 1] - d1;
+				}
+				else if (i == m2) 
+				{
+					days = days + d2;
+				}
+				else months = months + month[i - 1];		
+			}
+		}
+		else if (m2 == m1)
+		{
+			days = d2 - d1;
+		}
+	}
+	else if (y1 != y2)
+	{		
+		for (int i = y1; i < y2; i++)
+		{
+			LeapYear(i);
+			if (i == y1)
+			{
+
+				for (int i = m1; i <= 12; i++)
+				{
+					if (i == m1) days += month[i - 1] - d1;
+					else months += month[i - 1];
+				}
+				for (int i = 1; i <= m2; i++)
+				{
+					if (i == m2) days += d2;
+					else months += month[i - 1];
+				}
+			}
+			else years += y;			
+		}
+	}
+	sum = days + months + years;
+	return sum;
+}
 
 int main()
 {
-	int d1, d2, m1, y1, m2, y2, year, month = 0, day = 0, sum = 0;
-	int Month[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };	
-	cout << "Incert 2 dates in format dd mm yy:" << endl;
-	cout << "Incert 1st date: " << endl;
+	int month[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int d1, d2, m1, m2, y1, y2, days = 0, months = 0, years = 0, sum = 0;
+	cout << "Incert 2 dates in format dd mm yy begining from the lovest one: " << endl;
+	cout << "Incert 1-st date: " << endl;
 	cin >> d1 >> m1 >> y1;
-	cout << "Incert 2d date: " << endl;
+	cout << "Incert 2-d date: " << endl;
 	cin >> d2 >> m2 >> y2;
-	
+	sum = HowManyDays(d1, d2, m1, m2, y1, y2);
+	cout << "The number of days between 2 dates is: " << sum;
+	return 0;
 }
 
 
