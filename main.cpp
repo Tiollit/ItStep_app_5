95f552cb29b2efd7683b94d739d4915ffe74c7de
Tiollit <Tiollit7@gmail.com>
2022-11-04 15:25:11 -0700

    Lab 3

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <ctime>
 using namespace std;
 
-int HowMany(int arr[], int m, int p, int z)
-{
-    for (int i = 0; i < 20; i++)
+int HowMany(int arr[], int m, int p, int z, int n)
+{    
+    for (int i = 0; i < n; i++)
     {       
         if (arr[i] == 0) z += 1;
         else if (arr[i] < 0) m += 1;
-        else if (arr[i] > 0) p += 1;
-        return 0;
+        else if (arr[i] > 0) p += 1;       
+        return m, p, z;
     }
 }
 
@@ -17,14 +17,15 @@ int main()
 {
     srand(time(0));
     int arr[20];
-    int z = 0, p = 0, m = 0;
+    int m = 0, p = 0, z = 0;
     cout << "Generating array: " << endl;
     for (int i = 0;i < 20;i++)
     {
         arr[i] = rand() % 100 - 50;
         cout << arr[i] << " ";
     }
-    HowMany(arr, m, p, z);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    HowMany(arr, m, p, z, n);
     cout << endl;
     cout << "In array there is: " << z << " Zero elements; " << p << " Plus elements; " << m << " Minus elements";
             
