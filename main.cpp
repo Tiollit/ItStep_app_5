5842f83b2252ebefb0087fce1ac01c54e6132328
Tiollit <Tiollit7@gmail.com>
2022-11-04 09:07:43 -0700

    Lab 3 d 1

--- main.cpp
+++ main.cpp
@@ -1,123 +1,33 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-//void PrintArray(int arr[], const uint32_t size)
-//{
-//  for (size_t i = 0; i < size; i++)
-//  {
-//      cout << arr[i] << "\t";
-//  }cout << endl;
-//}
-//void PrintArray(double arr[], const uint32_t size)
-//{
-//  for (size_t i = 0; i < size; i++)
-//  {
-//      cout << arr[i] << "\t";
-//  }cout << endl;
-//}
-//void PrintArray(char arr[], const uint32_t size)
-//{
-//  for (size_t i = 0; i < size; i++)
-//  {
-//      cout << arr[i] << "\t";
-//  }cout << endl;
-//}
-template<typename T>
-T Rand(char trigger = 'i')
+
+int HowMany(int arr[], int m, int p, int z)
 {
-    switch (trigger)
-    {
-    case 'i':
-    {
-        return rand() % 10;
-    }break;
-    case 'd':
-    {
-        return (1000 + rand() % 9999) * 0.1;
-    }break;
-    case 'c':
-    {
-        return 100 + rand() % 255;
-    }break;
+    for (int i = 0; i < 20; i++)
+    {       
+        if (arr[i] == 0) z += 1;
+        else if (arr[i] < 0) m += 1;
+        else if (arr[i] > 0) p += 1;
+        return 0;
     }
 }
-template <typename T>
-void RandArray(T arr[], const uint32_t size)
-{
-    char trigger = '0';
-    if (typeid(arr[0]) == typeid(int))
-    {
-        trigger = 'i';
-    }
-    if (typeid(arr[0]) == typeid(double))
-    {
-        trigger = 'd';
-    }
-    if (typeid(arr[0]) == typeid(char))
-    {
-        trigger = 'c';
-    }
-    for (size_t i = 0; i < size; i++)
-    {
-        arr[i] = Rand<T>(trigger);
-    }
-}
-template <typename T>
-void RandArray(T arr[][5], const uint32_t col, const uint32_t row)
-{
-    char trigger = '0';
-    if (typeid(arr[0][0]) == typeid(int))
-    {
-        trigger = 'i';
-    }
-    if (typeid(arr[0][0]) == typeid(double))
-    {
-        trigger = 'd';
-    }
-    if (typeid(arr[0][0]) == typeid(char))
-    {
-        trigger = 'c';
-    }
-    for (size_t i = 0; i < col; i++)
-    {
-        for (size_t j = 0; j < row; j++)
-        {
-            arr[i][j] = Rand<T>(trigger);
-        }
-    }
-}
-template <typename T>
-void PrintArray(T arr[][5], const uint32_t col, const uint32_t row)
-{
-    for (size_t i = 0; i < col; i++)
-    {
-        for (size_t j = 0; j < row; j++)
-        {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-template <typename T>
-void PrintArray(T arr[], const uint32_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arr[i] << "\t";
-    }cout << endl;
-}
+
 int main()
 {
     srand(time(0));
-    const uint32_t size = 5;
-    int arr_i[size];
-    RandArray<int>(arr_i, size);
-    PrintArray<int>(arr_i, size);
+    int arr[20];
+    int z = 0, p = 0, m = 0;
+    cout << "Generating array: " << endl;
+    for (int i = 0;i < 20;i++)
+    {
+        arr[i] = rand() % 100 - 50;
+        cout << arr[i] << " ";
+    }
+    HowMany(arr, m, p, z);
     cout << endl;
-    double arr_d[size][size];
-    RandArray<double>(arr_d, size, size);
-    PrintArray<double>(arr_d, size, size);
+    cout << "In array there is: " << z << " Zero elements; " << p << " Plus elements; " << m << " Minus elements";
+            
     return 0;
 }
 
