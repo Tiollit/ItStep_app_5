a0918ee1caa26e6eab8f72e573af8be400ef2d5b
Tiollit <Tiollit7@gmail.com>
2022-11-06 08:51:34 -0800

    Lab 10 c

--- main.cpp
+++ main.cpp
@@ -1,30 +1,58 @@
 #include <iostream>
-#include<iomanip>
+#include <iomanip>
 #include <ctime>
-#include<stdlib.h>
+#include <stdlib.h>
 using namespace std;
-#define n 4;
 
 int Matrix(int arr[])
 {
-    for (int i = 0;i < n; i++)
+    int arr[4][4];
+    for (size_t i = 0;i < 4; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (size_t j = 0; j < 4; j++)
         {
             arr[i][j] = rand() % 100;
+            cout << arr[i][j] << " ";
         }
+        cout << endl;
     }
+    return arr[4][4];
 }
 
 double Matrix(double arr[])
-{    
-    for (int i = 0;i < n; i++)
+{
+    double arr[4][4];
+    for (size_t i = 0;i < 4; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (size_t j = 0; j < 4; j++)
         {
             arr[i][j] = rand() % 100 + 0.1;
+            cout << arr[i][j] << " ";
         }
+        cout << endl;
     }
+    return arr[4][4];
+}
+
+char Matrix(char arr[])
+{
+    char arr[4][4];
+    arr[4][4] =
+    {
+        {'h', 'e', 'l', 'l'},
+        {'b', 'a', 'c', 'k'},
+        {'r', 'e', 'a', 'l'},
+        {'f', 'a', 'l', 'l'}
+    };
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return arr[4][4];
 }
 
 int main()
